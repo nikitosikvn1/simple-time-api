005fe45ea3d6c0ee7100515daeed8bc5e1057519
YanPetrov7 <yanemerald2004@gmail.com>
2023-02-20 10:39:52 +0200

    feat: main func

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
+	"log"
 )
 
 type TimeResponse struct {
@@ -13,7 +14,22 @@ type TimeResponse struct {
 func currentTimeHandler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC3339)
 	timeResponse := TimeResponse{Time: currentTime}
+
 	timeResponseJSON, err := json.Marshal(timeResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(timeResponseJSON)
 }
 
-func main() {}
+func main() {
+    http.HandleFunc("/time", currentTimeHandler)
+
+    err := http.ListenAndServe(":8795", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
